4928cf0bf3f877b3d8c38a4d7191d9aff8b17988
SunilKumar02 <75972320+SunilKumar02@users.noreply.github.com>
2021-09-20 14:22:47 +0530

    Routing and Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.scss';
 import HomePage from './pages/homePage/homePage.component'
+import ShopPage from './pages/shop/shop.component'
+import { Route ,Switch } from 'react-router-dom';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage}></Route>
+        <Route path="/shop" component={ShopPage}></Route>
+      </Switch>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,8 +1,13 @@
-import react from "react";
+import React from "react";
+import { withRouter } from "react-router-dom";
 import './menu-item.style.scss'
 
-const MenuItem = ({title, imageUrl, size}) => (
-    <div className={`${size} menu-item`}>
+const MenuItem = ({title, imageUrl, size, history, match, linkUrl }) => (
+    <div 
+    className={`${size} menu-item`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+    >
+
       <div
       className='background-image'
       style={{backgroundImage: `url(${imageUrl})`}}
@@ -10,10 +15,10 @@ const MenuItem = ({title, imageUrl, size}) => (
 
       </div>
         <div className='content'>
-          <div className='title'>{title}</div>
+          <div className='title'>{title.toUpperCase()}</div>
           <span className='subtitle'>SHOP NOW</span>
         </div>
     </div>
 )
 
-export default MenuItem;
+export default withRouter(MenuItem);
